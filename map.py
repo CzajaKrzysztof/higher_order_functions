31573bd3d608c3618569661705ce9ebd0f38b975
Krzysiek Czaja <krzysiekczaja@gmail.com>
2019-01-09 20:03:43 +0100

    Initial commit

---
+++ map.py
@@ -0,0 +1,11 @@
+def get_user_iterable():
+    iterable = input("Enter list of items to apply function to: ")
+    return iterable
+
+
+def main():
+    iterable = get_user_iterable()
+
+
+if __name__ == '__main__':
+    main()
