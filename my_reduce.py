3d9c0e0f94d45266ce2828ec0d1c099e6bb0493d
Krzysiek Czaja <krzysiekczaja@gmail.com>
2019-01-10 11:34:24 +0100

    Implement functionality of custom reduce() function my_reduce()

--- my_reduce.py
+++ my_reduce.py
@@ -1,4 +1,17 @@
+def my_reduce(function, iterate):
+    if len(iterate) == 2:
+        result = sum(iterate)
+    else:
+        new_iterate = iterate[2:]
+        new_iterate.insert(0, function(iterate[0], iterate[1]))
+        result = my_reduce(function, new_iterate)
+
+    return result
+
+
 def main():
+    my_iterate = [47, 11, 42, 13]
+    print(my_reduce(lambda x, y: x + y, my_iterate))
 
 
 if __name__ == '__main__':
