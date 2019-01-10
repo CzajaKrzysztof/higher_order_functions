f8d0ecfd296f254d8d14b79efeee3057c4a60eec
Krzysiek Czaja <krzysiekczaja@gmail.com>
2019-01-10 10:54:24 +0100

    Initial commit for custom filter() function

---
+++ my_filter.py
@@ -0,0 +1,12 @@
+def my_filter(function, iterator):
+    result = []
+    pass
+
+
+def main():
+    my_list = [1, 5, 3, 7, 8, 3]
+    print(my_filter(funcition, my_list))
+
+
+if __name__ == '__main__':
+    main()
