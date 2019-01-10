e92380e462034ceb6f3e53dae1dc2c3cb10d1ef0
Krzysiek Czaja <krzysiekczaja@gmail.com>
2019-01-10 10:53:50 +0100

    support for myltiple iterators in custom map() function

--- map.py
+++ map.py
@@ -1,14 +1,12 @@
 def my_map(function, *args):
     result = []
-    print(args)
-    print(list(zip(args)))
     if len(args) == 1:
         for i in args[0]:
             result.append(function(i))
     else:
-        list_of_args = list(zip(args))
-        # for i in list_of_args:
-        #     result.append(function(i[0], i[1]))
+        list_of_args = list(zip(*args))
+        for i in list_of_args:
+            result.append(function(*i))
 
     return result
 
@@ -16,7 +14,8 @@ def my_map(function, *args):
 def main():
     input_list_one = [2, 5, 6, 1, 9, 6, 3]
     input_list_two = [5, 2, 5, 6, 7, 1, 5]
-    print(my_map(lambda x, y: x + y, input_list_one, input_list_two))
+    input_list_tree = [1, 2, 5, 7, 7, 3, 4]
+    print(my_map(lambda x, y, z: x + y + z, input_list_one, input_list_two, input_list_tree))
 
 
 if __name__ == '__main__':
