796ce26db06681dcc34df3eb43a4e71e960e0933
Krzysiek Czaja <krzysiekczaja@gmail.com>
2019-01-10 09:17:44 +0100

    support for single list in map function

--- map.py
+++ map.py
@@ -1,10 +1,22 @@
-def get_user_iterable():
-    iterable = input("Enter list of items to apply function to: ")
-    return iterable
+def my_map(function, *args):
+    result = []
+    print(args)
+    print(list(zip(args)))
+    if len(args) == 1:
+        for i in args[0]:
+            result.append(function(i))
+    else:
+        list_of_args = list(zip(args))
+        # for i in list_of_args:
+        #     result.append(function(i[0], i[1]))
+
+    return result
 
 
 def main():
-    iterable = get_user_iterable()
+    input_list_one = [2, 5, 6, 1, 9, 6, 3]
+    input_list_two = [5, 2, 5, 6, 7, 1, 5]
+    print(my_map(lambda x, y: x + y, input_list_one, input_list_two))
 
 
 if __name__ == '__main__':
