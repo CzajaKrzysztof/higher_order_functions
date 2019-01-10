c6a04520634f27acfd419067182486bfe9a5df58
Krzysiek Czaja <krzysiekczaja@gmail.com>
2019-01-10 11:08:16 +0100

    Implement functionality to custom my_filter function

--- my_filter.py
+++ my_filter.py
@@ -1,11 +1,21 @@
 def my_filter(function, iterator):
-    result = []
-    pass
+    result = [item for item in iterator if function(item)]
+
+    return result
 
 
 def main():
-    my_list = [1, 5, 3, 7, 8, 3]
-    print(my_filter(funcition, my_list))
+    my_list = [1, 5, 3, 7, 8, 3, 10, 4, 0]
+    print("Input list: {0}".format(my_list))
+    # Function examples for demo purpose
+    print("\nlambda x: x % 2 == 0")
+    print(my_filter(lambda x: x % 2 == 0, my_list))
+
+    print("\nlambda x: x % 2 != 0")
+    print(my_filter(lambda x: x % 2 != 0, my_list))
+
+    print("\nlambda x: x != 4")
+    print(my_filter(lambda x: x != 4, my_list))
 
 
 if __name__ == '__main__':
